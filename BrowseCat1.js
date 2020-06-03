1c47c85c2893c770d4ffa9dcedee3a070f6da0cb
gargipandkar <54140892+gargipandkar@users.noreply.github.com>
2020-06-03 22:40:24 +0800

    Update BrowseCat1.js

--- BrowseCat1.js
+++ BrowseCat1.js
@@ -43,8 +43,8 @@ class BrowseCat1 extends Component {
                <ScrollView>
                   {
                      this.state.names.map((item, index) => (
-                        <View key = {item.name} style = {styles.item}>
-                           <Text onPress={() => console.log(item.name)}>{item.name}</Text>
+                        <View key = {item.key} style = {styles.item}>
+                           <Text onPress={() => console.log(item.storename)}>{item.storename}</Text>
                         </View>
                      ))
                   }
