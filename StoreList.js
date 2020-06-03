5f4efec9ddee675e66e9c99674e033e0a04555ac
Clement <clement_ravindran@mymail.sutd.edu.sg>
2020-06-04 01:38:20 +0800

    Fixing Errors

--- StoreList.js
+++ StoreList.js
@@ -11,29 +11,38 @@ import {
   TextInput,
 } from 'react-native';
 
-import {db} from './fb.config';
+import {db} from './App';
 
-allretailers = [];
 
-db.ref('/Restaurant')
-  .once('value')
-  .then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      //var key=childSnapshot.key;
-      var val = childSnapshot.val();
-      allretailers.push(val);
-    });
-    //this.datacopy=allretailers;
-    //console.log("exist?", this.datacopy);
-  });
+export default class StoreList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+      text: '',
+      data: [],
+    };
 
-export default class SearchableList extends Component {
-  state = {
-    isLoading: true,
-    text: '',
-    data: allretailers,
-  };
-  datacopy = allretailers;
+    this.setupFirebaseListener()
+ }
+  setupFirebaseListener = () => {
+    db.ref('/Restaurant')
+      .once('value')
+      .then(function(snapshot) {
+        allretailers = [];
+        snapshot.forEach(function(childSnapshot) {
+          //var key=childSnapshot.key;
+          var val = childSnapshot.val();
+          allretailers.push(val);
+        });
+        this.setState({
+          ...this.state,
+          data: allretailers
+        })
+        //this.datacopy=allretailers;
+        //console.log("exist?", this.datacopy);
+      });
+ }
 
   componentDidMount() {
     this.setState({isLoading: false});
